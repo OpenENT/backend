dcf02b7250ac519c97420b5f8406f15a2f1b2ed5
Mast03 <antoniobased@icloud.com>
2021-08-20 17:37:04 +0200

    Update test provider

--- app/providers/test_provider.py
+++ app/providers/test_provider.py
@@ -1,6 +1,13 @@
 from provider import Provider
+import structs
 
 class Test_Provider(Provider):
 
     def __init__(self):
         super().__init__(name='Test')
+
+    def search(self, query: str):
+        artist = structs.Artist(name="PinocchioP", img_url="https://static.wikia.nocookie.net/vocaloid/images/d/d0/PinocchioDoushite.jpg/revision/latest?cb=20210705085920")
+        album = structs.Album(title="Human", img_url="https://static.wikia.nocookie.net/vocaloid/images/0/07/Human.jpg/revision/latest?cb=20170706215339", artist=artist)
+        song = structs.Song("http://127.0.0.1:5000/usorasera.mp3", title="ウソラセラ", album=album)
+        return (artist, album, song, song, song)
