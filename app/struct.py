04d5657308342282eba1723a68d173f063fe4671
Mast03 <antoniobased@icloud.com>
2021-08-20 17:36:59 +0200

    Rename and fix structs

--- app/struct.py
+++
@@ -1,9 +0,0 @@
-
-def Artist(name="Unknown", img_url=None, tags=None):
-    return {"name": name, "img_url": img_url, "tags": tags}
-
-def Album(title="Unknown", img_url=None, tags=None, artist=Artist()):
-    return {"title": title, "img_url": img_url, "artist": artist, "tags": tags}
-
-def Song(stream_url, title="Unknown", album=Album()):
-    return {"title": title, "stream_url": stream_url, "album": album}
---
+++ app/structs.py
@@ -0,0 +1,9 @@
+
+def Artist(name="Unknown", img_url=None, tags=None):
+    return {"type": "artist", "name": name, "img_url": img_url, "tags": tags}
+
+def Album(title="Unknown", img_url=None, tags=None, artist=Artist()):
+    return {"type": "album", "title": title, "img_url": img_url, "artist": artist, "tags": tags}
+
+def Song(stream_url, title="Unknown", album=Album()):
+    return {"type": "song", "title": title, "stream_url": stream_url, "album": album}
