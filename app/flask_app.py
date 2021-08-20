3191624e64f3d8b6c03d1a65e7e5933369824807
Mast03 <antoniobased@icloud.com>
2021-08-20 12:10:21 +0200

    Bogus search and completed info callback

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from providers.test_provider import Test_Provider
 import json
 
 app = Flask(__name__)
+providers = (Test_Provider(),)
 
 @app.route('/')
 def root():
@@ -11,5 +13,14 @@ def root():
 
 @app.route('/info')
 def info():
-    test = request.args.get('test')
+    names = list()
+    for provider in providers:
+        names.append(provider.name)
+    return {'providers': names}
+
+@app.route('/search')
+def search():
+    plugin = request.args.get('plugin')
+    query = request.args.get('query')
     return 'test'
+
