94abb6a1ea57ea6841eb7204f770129f789fd184
Mast03 <antoniobased@icloud.com>
2021-08-31 14:44:10 +0200

    Add mandatory provider field

--- app/structs.py
+++ app/structs.py
@@ -5,5 +5,5 @@ def Artist(name="Unknown", img_url=None, tags=None):
 def Album(title="Unknown", img_url=None, tags=None, artist=Artist()):
     return {"type": "album", "title": title, "img_url": img_url, "artist": artist, "tags": tags}
 
-def Song(stream_url, title="Unknown", album=Album()):
-    return {"type": "song", "title": title, "stream_url": stream_url, "album": album}
+def Song(stream_url, provider, title="Unknown", album=Album()):
+    return {"type": "song", "title": title, "stream_url": stream_url, "provider": provider, "album": album}
