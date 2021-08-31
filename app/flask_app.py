3b2c8364267b16ac1855360156ea9b0c4cec802c
Mast03 <antoniobased@icloud.com>
2021-08-31 14:49:15 +0200

    Download call

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-from providers.test_provider import Test_Provider
+from flask import send_from_directory
 from providers.youtube import YoutubeProvider
 from providers.nicovideo import NicoProvider
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 providers = (YoutubeProvider(), NicoProvider())
 
 @app.route('/')
@@ -28,3 +28,19 @@ def search():
         if provider in p.name:
             return {'response': 200, 'result': p.search(query=query)}
     return {'response': 404}
+
+@app.route('/download')
+def download():
+    provider = request.args.get('provider')
+    stream_url = request.args.get('stream_url')
+    for p in providers:
+        if provider in p.name:
+            url = p.download(stream_url)
+            if url is None:
+                return {'response': 500}
+            return {'response': 200, 'url': f"http://127.0.0.1:5001/streams/{url}"}
+    return {'response': 404}
+
+@app.route('/streams/<path:path>')
+def send_js(path):
+    return send_from_directory('streams', path)
