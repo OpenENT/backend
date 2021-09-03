d8b56553d5536dd4ea90605bcef2a3f275c91bb1
Dx55 <luigicesaro2003@libero.it>
2021-09-03 18:15:33 +0200

    Search fix

--- app/providers/soundcloud.py
+++ app/providers/soundcloud.py
@@ -12,15 +12,17 @@ class SoundcloudProvider(Provider):
     
     def search(self, query: str):
         res = list()
-        req = requests.get(f"{HOST}/search?q={query}")
+        req = requests.get(f"{HOST}/search?q={query.replace('+', '%20')}")
         soup =  BeautifulSoup(req.content, 'html.parser')
         # seems weird but it works
         link_list = list()
         title_list = list()
+        query = query.replace(' ', '-')
         for data in soup.select(f'a[href*={query.lower()}]'):
             link_list.append(''.join([HOST, data['href']]))
             title_list.append(data.text)
-        for i in range(10):
+        # shitty fix for some results that can't be included in the range(10)
+        i = 0
+        while i >= 10:
             res.append(structs.Song(link_list[i], provider="Soundcloud", title=title_list[i], album="WIP"))
         return res
-
