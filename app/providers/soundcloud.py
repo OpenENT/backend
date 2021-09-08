8f558aad4e5afdae81f71e08e8fbe2864cc6f5c5
Mast03 <antoniobased@icloud.com>
2021-09-08 23:35:12 +0200

    [Soundcloud] Code cleanup

--- app/providers/soundcloud.py
+++ app/providers/soundcloud.py
@@ -8,7 +8,7 @@ import requests
 HOST = "https://soundcloud.com"
 
 class SoundcloudProvider(Provider):
-    
+
     def __init__(self):
         super().__init__(name='Soundcloud')
     
@@ -16,24 +16,19 @@ class SoundcloudProvider(Provider):
         res = list()
         req = requests.get(f"{HOST}/search?q={query.replace('+', '%20')}")
         soup =  BeautifulSoup(req.content, 'html.parser')
-        # seems weird but it works
-        link_list = list()
-        title_list = list()
-        query = query.replace(' ', '-')
-        query = query.replace("'", "")
+        dikt = dict()
+        query = query.replace(' ', '-').replace("'", "")
         try:
             for data in soup.select(f'a[href*={query.lower()}]'):
-                link_list.append(''.join([HOST, data['href']]))
-                title_list.append(data.text)
-            for i in range(len(link_list[:10])):
-                res.append(structs.Song(link_list[i], provider=self.name, title=title_list[i], album="WIP"))
-        except:
-            pass
+                dikt[''.join([HOST, data['href']])] = data.text
+            for url, title in dikt.items():
+                res.append(structs.Song(url, provider=self.name, title=title))
+        except Exception as e:
+            print(e)
         return res
     
     def download(self, stream_url: str):
-        id = stream_url[23:]
-        id = id.replace('/', '-')
+        id = stream_url[23:].replace('/', '-')
         command = f"yt-dlp --output streams/{id}.%\(ext\)s -f hls_opus_64 --embed-metadata --embed-thumbnail {stream_url}"
         process = subprocess.Popen(command, shell=True)
         process.wait()
