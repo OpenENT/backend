2afdd2fa2b8e584ab5401233674fc6852229e925
Mast03 <antoniobased@icloud.com>
2021-10-05 09:43:47 +0200

    [Nicovideo] Gather author data (debug)

--- app/providers/nicovideo.py
+++ app/providers/nicovideo.py
@@ -1,11 +1,13 @@
-from pathlib import Path
 from provider import Provider
+from pathlib import Path
 
-import json
+import subprocess
+import flask_app
 import requests
 import structs
-import subprocess
+import json
 
+HOST_USERAPI = "https://public.api.nicovideo.jp/v1/timelines/nicorepo/last-6-months/users/%s/pc/entries.json"
 HOST = "https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search"
 
 class NicoProvider(Provider):
@@ -15,16 +17,23 @@ class NicoProvider(Provider):
 
     def search(self, query: str):
         res = list()
-        req = requests.get(f"{HOST}?q={query}&targets=title,tags&fields=contentId,title,tags,thumbnailUrl&_sort=-viewCounter&_offset=0&_limit=10&_context=niconico")
+        req = requests.get(f"{HOST}?q={query}&targets=title,tags&fields=userId,contentId,title,tags,thumbnailUrl&_sort=-viewCounter&_offset=0&_limit=10&_context=niconico")
         content = json.loads(req.content)
-        
         for i in range(len(content["data"])):
             id = content["data"][i]["contentId"]
             title = content["data"][i]["title"] 
             tags = content["data"][i]["tags"]
             thumbnail = content["data"][i]["thumbnailUrl"]
+            userId = content["data"][i]["userId"]
             artist = structs.Artist()
-            album = structs.Album(title=title)
+            if flask_app.settings.debug: # Needs caching ig
+                req2 = requests.get(HOST_USERAPI % userId)
+                if req2.status_code == 200:
+                    c = json.loads(req2.text)
+                    if len(c['data']) > 0:
+                        artist['name'] = c['data'][0]['actor']['name']
+                        artist['img_url'] = c['data'][0]['actor']['icon']
+            album = structs.Album(title=title, artist=artist)
             song = structs.Song("https://nicovideo.jp/watch/"+id, provider=self.name, title=title, album=album)
             res.append(song)
         return res
